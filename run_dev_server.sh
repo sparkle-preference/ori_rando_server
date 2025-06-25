8284054deaadb2bed08e188ec05897a32ca43b20
Sydney <sydney@hyphen-emdash.com>
2025-06-25 15:09:31 -0700

    Allow for build but no watch

--- run_dev_server.sh
+++ run_dev_server.sh
@@ -1,8 +1,14 @@
-if [ "${NO_WATCH_REACT_APP}" != "1" ]; then
+if [ "${NO_BUILD_REACT_APP}" != "1" ]; then
     cd map
-    npm run watch &
+    if [ "${NO_WATCH_REACT_APP}" == "1" ]; then
+        npm run build
+    else
+        # Parcel is almost always faster than appengine
+        npm run watch &
+    fi
     cd ../
 fi
+
 
 
 if [ -z "${GCLOUD_CLI_ROOT}" ]; then
