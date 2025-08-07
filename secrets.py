470de2a52c4216c502c1fd3b606f821a69933af2
Sydney <sydney@hyphen-emdash.com>
2025-08-07 14:23:09 -0700

    Migrate to OIDC

--- secrets.py
+++ secrets.py
@@ -1,1 +1,8 @@
 whitelist_secret = "PLACEHOLDER_DEV_SECRET"
+#Insecure Dev Secret Key. do not use in production
+app_secret_key = b'INSECURE_DEV_SECRET_KEY'
+
+raise """Delete this line to acknowledge that you've generated a new secret key if you're in production. 
+you can use
+python -c 'import secrets; print(secrets.token_hex())'
+to generate a new, secure secret key"""
