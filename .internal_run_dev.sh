29dfb1975a8ccf9b5acb3b3d46776f5ed3ac80bb
Sydney <sydney@hyphen-emdash.com>
2025-08-12 14:53:04 -0700

    no more GAE

---
+++ .internal_run_dev.sh
@@ -0,0 +1,6 @@
+if [ ! -e /tmp/pipcache ] || [ cmp -s "requirements.txt" "/tml/pipcache" ]; then
+    cp requirements.txt /tmp/pipcache
+    pip install -r requirements.txt
+fi
+
+python -m flask --debug --app main run --host="0.0.0.0"
