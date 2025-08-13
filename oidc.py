242874fb3d2db2dfb3c15316451892580f41ea01
Sydney <sydney@hyphen-emdash.com>
2025-08-12 22:54:46 -0700

    Fix issues with OIDC

--- oidc.py
+++ oidc.py
@@ -5,6 +5,7 @@ from flask_oidc import OpenIDConnect
 
 def make_oidc(app):
     app.config["OIDC_CLIENT_SECRETS"] = os.getenv("OIDC_CLIENT_SECRETS", "oauth/client_secret.json")
+    app.config["OIDC_OVERWRITE_REDIRECT_URI"] = os.getenv("OIDC_OVERWRITE_REDIRECT_URI", "https://orirando.com/authorize")
     if debug():
         app.config["OIDC_ENABLED"] = os.getenv("OIDC_ENABLED", "False") == "True"
         app.config["OIDC_TESTING_PROFILE"] = {
