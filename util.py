55c7d03acc642604342c4925f7e32bac4f3b64e3
Eiko <lapis.catzuli@gmail.com>
2026-07-30 20:04:31 -0700

    ver bump (optional tho since I'vebeen doing a lot of these)

--- util.py
+++ util.py
@@ -23,9 +23,9 @@ try:
 except ImportError:
     ndb_imported = False
 
-VER = [4, 2, 4]
+VER = [4, 2, 5]
 MIN_VER = [4, 2, 4]
-BETA_VER = [4, 2, 4]
+BETA_VER = [4, 2, 5]
 
 # Feature flags for netcode rework (graceful fallback: unset/0 = legacy behavior)
 def _flag(name):
